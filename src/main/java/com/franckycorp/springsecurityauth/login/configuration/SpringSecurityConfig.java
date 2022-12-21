bbbe66a475e8b4624b377fc5e4efd39d6643851c
FranckPedron <franckyp1+rapports_git@gmail.com>
2022-12-21 17:20:54 +0100

    Build - add roles handling

--- src/main/java/com/franckycorp/springsecurityauth/login/configuration/SpringSecurityConfig.java
+++ src/main/java/com/franckycorp/springsecurityauth/login/configuration/SpringSecurityConfig.java
@@ -3,6 +3,11 @@ package com.franckycorp.springsecurityauth.login.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,21 +25,26 @@ public class SpringSecurityConfig {
     }
 
     @Bean
-    public UserDetailService userDetailService(BCryptPasswordEncoder bCryptPasswordEncoder) {
+    public UserDetailsService userDetailService() {
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
 
         manager.createUser(User
                 .withUsername("springuser")
-                .password(bCryptPasswordEncoder.encode("spring123"))
+                .password(passwordEncoder().encode("spring123"))
                 .roles("USER")
                 .build());
 
         manager.createUser(User
                 .withUsername("springadmin")
-                .password(bCryptPasswordEncoder.encode("admin123"))
+                .password(passwordEncoder().encode("admin123"))
                 .roles("ADMIN","USER")
                 .build());
 
         return manager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
